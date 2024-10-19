6cc896e5d2230d76c42a899f4beac29d893da5d8
Noe Minidlaia <134667441+gitgodgit@users.noreply.github.com>
2024-10-19 14:10:42 +0400

    Update Task1.py

--- homework_1/Task1.py
+++ homework_1/Task1.py
@@ -1,1 +1,6 @@
 
+name = input("please enter your name: ")
+last_name = input("please enter your last name: ")
+age = input("please enter your age: ")
+
+print("Hello", name, last_name, "you are", age, "years old")
